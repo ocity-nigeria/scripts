6bab0e8c87bdbf433a03f82696f222ef09489d8f
Sunday <sundaymba91@gmail.com>
2023-04-22 17:19:05 +0100

    laravel setup

--- laravel-setup.sh
+++ laravel-setup.sh
@@ -23,7 +23,9 @@ sudo mysql_secure_installation
 
 
 # Get the MySQL root password from the user
-read -s -p "Enter MySQL root password: " root_password
+read -s -p "Enter MySQL root password: " root_password'
+read -s -p "Enter Dialogflux database user name: " databaseUser'
+read -s -p "Enter Dialogflux database user password: " databaseUserPassword'
 
 # Update the package list
 sudo apt-get update
@@ -38,11 +40,13 @@ sudo mysql_secure_installation
 mysql -u root -p"$root_password" <<EOF
 
 # Create a new MySQL user and grant them privileges
-CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'newuser'@'localhost' WITH GRANT OPTION;
+CREATE USER '$databaseUser'@'localhost' IDENTIFIED BY '$databaseUserPassword';
+GRANT ALL PRIVILEGES ON *.* TO '$databaseUser'@'localhost' WITH GRANT OPTION;
 
 # Exit MySQL
 EXIT
 EOF
 
-echo "MySQL user 'newuser' with password 'password' has been created."
+echo "MySQL user '$databaseUser' with password 'password' has been created. PHP and NGINX Installed"
+
+ 
